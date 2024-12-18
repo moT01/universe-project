99eaacf7a03c6fc15a145333cd4884babda269eb
Carmine Porpora <porpora.carmine.lavoro@gmail.com>
2024-12-18 22:59:33 +0000

    test

--- universe.sql
+++ universe.sql
@@ -3,7 +3,7 @@
 --
 
 -- Dumped from database version 12.17 (Ubuntu 12.17-1.pgdg22.04+1)
--- Dumped by pg_dump version 12.17 (Ubuntu 12.17-1.pgdg22.04+1)
+-- Dumped by pg_dump version 12.17 (Ubuntu 12.17-1.pgdg22.04+1)n
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
